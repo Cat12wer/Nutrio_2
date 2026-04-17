16aede99198907eadc3855aebe6ed88e5b73f14e
Kosta <obintov@gmail.com>
2026-04-17 20:26:43 +0300

    Second commit

--- Nutrio_Domain/Nutrio_Domain/Common/Entity.cs
+++ Nutrio_Domain/Nutrio_Domain/Common/Entity.cs
@@ -2,17 +2,10 @@
 
 public abstract class Entity<TId>
 {
-    public TId Id { get; protected set; }
+    public TId Id { get; protected set; } = default!;
 
     // Для сутностей, де ми хочемо самі керувати ID (наприклад, Guid)
     protected Entity() { }
-
-    // Конструктор для випадків, коли ID приходить ззовні (наприклад, int для Product)
-    protected Entity(TId id)
-    {
-        Id = id;
-    }
-
     public override bool Equals(object? obj)
     {
         if (obj is not Entity<TId> other) return false;
